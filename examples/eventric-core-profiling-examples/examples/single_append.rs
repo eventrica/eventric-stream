d21d379df044802af33517550cf5d5c136698eaa
Andrew Cherry <andrew@xyncro.com>
2025-10-26 23:00:29 +0000

    profiling examples

---
+++ examples/eventric-core-profiling-examples/examples/single_append.rs
@@ -0,0 +1,49 @@
+use std::time::Instant;
+
+use eventric_core::{
+    Data,
+    Error,
+    Event,
+    Identifier,
+    Stream,
+    Tag,
+    Version,
+};
+
+// =================================================================================================
+// Single Append
+// =================================================================================================
+
+// Configuration
+
+static PATH: &str = "./temp";
+
+// -------------------------------------------------------------------------------------------------
+
+// Profile
+
+#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+pub fn main() -> Result<(), Error> {
+    let mut stream = Stream::builder(PATH).temporary(true).open()?;
+
+    let events = [Event::new(
+        Data::new("Hello World".bytes().collect()),
+        Identifier::new("test_identifier".into()),
+        Vec::from_iter([Tag::new("test_tag_a".into()), Tag::new("test_tag_b".into())]),
+        Version::new(0),
+    )];
+
+    let iterations = 1_000_000f64;
+    let start = Instant::now();
+
+    for _ in 0..iterations as u64 {
+        stream.append(&events, None).unwrap();
+    }
+
+    let duration = Instant::now().duration_since(start);
+    let events_per_sec = (iterations / duration.as_secs_f64()) as u64;
+
+    println!("time: {duration:?} ({events_per_sec} events/sec)",);
+
+    Ok(())
+}
