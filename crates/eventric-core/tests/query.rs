a1a85f2d672cbf17ab2260da5f806bdd3ad9eaa8
Andrew Cherry <andrew@xyncro.com>
2025-10-31 16:06:36 +0000

    test refactoring

--- crates/eventric-core/tests/query.rs
+++ crates/eventric-core/tests/query.rs
@@ -1,3 +1,8 @@
+use std::{
+    path::Path,
+    sync::LazyLock,
+};
+
 use assertables::{
     assert_none,
     assert_some,
@@ -30,11 +35,9 @@ use eventric_core::{
 
 #[test]
 fn new() -> Result<(), Error> {
-    let stream = Stream::builder(eventric_core::temp_path())
-        .temporary(true)
-        .open()?;
-
+    let stream = stream(eventric_core::temp_path(), false)?;
     let cache = Cache::default();
+
     let condition = Condition::default();
 
     let mut events = stream.query(&condition, &cache, None);
@@ -49,55 +52,10 @@ fn new() -> Result<(), Error> {
 }
 
 #[test]
-fn condition() -> Result<(), Error> {
-    let mut stream = Stream::builder(eventric_core::temp_path())
-        .temporary(true)
-        .open()?;
-
-    // New stream should always have a length of zero and be empty
-
-    {
-        assert_eq!(0, stream.len());
-        assert!(stream.is_empty());
-    }
-
-    let event_0 = EphemeralEvent::new(
-        Data::new("data_a")?,
-        Identifier::new("id_a")?,
-        [Tag::new("tag_1")?, Tag::new("tag_2")?, Tag::new("tag_3")?],
-        Version::new(0),
-    );
-
-    let event_1 = EphemeralEvent::new(
-        Data::new("data_b")?,
-        Identifier::new("id_b")?,
-        [Tag::new("tag_2")?, Tag::new("tag_3")?, Tag::new("tag_4")?],
-        Version::new(1),
-    );
-
-    let event_2 = EphemeralEvent::new(
-        Data::new("data_c")?,
-        Identifier::new("id_c")?,
-        [Tag::new("tag_3")?, Tag::new("tag_4")?, Tag::new("tag_5")?],
-        Version::new(0),
-    );
-
-    let event_3 = EphemeralEvent::new(
-        Data::new("data_d")?,
-        Identifier::new("id_c")?,
-        [Tag::new("tag_4")?, Tag::new("tag_5")?, Tag::new("tag_6")?],
-        Version::new(1),
-    );
-
-    let position = stream.append([&event_0, &event_1, &event_2, &event_3], None)?;
-
-    // Position should be 3 after appending 4 test events
-
-    {
-        assert_eq!(3, *position);
-    }
-
+fn default() -> Result<(), Error> {
+    let stream = stream(eventric_core::temp_path(), true)?;
     let cache = Cache::default();
+
     let condition = Condition::default();
 
     let mut events = stream.query(&condition, &cache, None);
@@ -114,7 +72,15 @@ fn condition() -> Result<(), Error> {
         assert_none!(events.next());
     }
 
-    let specifier = Specifier::new(event_2.identifier().clone(), None);
+    Ok(())
+}
+
+#[test]
+fn specifier() -> Result<(), Error> {
+    let stream = stream(eventric_core::temp_path(), true)?;
+    let cache = Cache::default();
+
+    let specifier = Specifier::new(EVENT_2.identifier().clone(), None);
     let selector = Selector::specifiers([specifier])?;
     let query = Query::new([selector])?;
     let condition = Condition::default().matches(&query);
@@ -127,18 +93,26 @@ fn condition() -> Result<(), Error> {
 
     {
         let event = assert_some_as_result!(events.next()).unwrap()?;
-        assert_eq!(event_2.data(), event.data());
+        assert_eq!(EVENT_2.data(), event.data());
         assert_eq!(2, **event.position());
 
         let event = assert_some_as_result!(events.next()).unwrap()?;
-        assert_eq!(event_3.data(), event.data());
+        assert_eq!(EVENT_3.data(), event.data());
         assert_eq!(3, **event.position());
 
         assert_none!(events.next());
     }
 
+    Ok(())
+}
+
+#[test]
+fn specifier_with_range() -> Result<(), Error> {
+    let stream = stream(eventric_core::temp_path(), true)?;
+    let cache = Cache::default();
+
     let range = Version::new(1)..Version::MAX;
-    let specifier = Specifier::new(event_2.identifier().clone(), Some(range));
+    let specifier = Specifier::new(EVENT_2.identifier().clone(), Some(range));
     let selector = Selector::specifiers([specifier])?;
     let query = Query::new([selector])?;
     let condition = Condition::default().matches(&query);
@@ -147,37 +121,209 @@ fn condition() -> Result<(), Error> {
 
     // A query with a single specifier with a version range should only return
     // events matching both the specifier and the range, in this case the single
-    // event with
+    // event with a matching version (1..)
 
     {
         let event = assert_some_as_result!(events.next()).unwrap()?;
-        assert_eq!(event_3.data(), event.data());
+        assert_eq!(EVENT_3.data(), event.data());
         assert_eq!(3, **event.position());
 
         assert_none!(events.next());
     }
 
-    // Query Multiple Specifiers
+    Ok(())
+}
 
-    let specifier_0 = Specifier::new(event_0.identifier().clone(), None);
-    let specifier_1 = Specifier::new(event_1.identifier().clone(), None);
+#[test]
+fn multiple_specifiers() -> Result<(), Error> {
+    let stream = stream(eventric_core::temp_path(), true)?;
+    let cache = Cache::default();
+
+    let specifier_0 = Specifier::new(EVENT_0.identifier().clone(), None);
+    let specifier_1 = Specifier::new(EVENT_1.identifier().clone(), None);
     let selector = Selector::specifiers([specifier_0, specifier_1])?;
     let query = Query::new([selector])?;
     let condition = Condition::default().matches(&query);
 
     let mut events = stream.query(&condition, &cache, None);
 
+    // A query with multiple specifiers (without version ranges in this case) should
+    // return events which match any of the specifiers, in this case the two events
+    // matching the identifiers supplied
+
     {
         let event = assert_some_as_result!(events.next()).unwrap()?;
-        assert_eq!(event_0.data(), event.data());
+        assert_eq!(EVENT_0.data(), event.data());
         assert_eq!(0, **event.position());
 
         let event = assert_some_as_result!(events.next()).unwrap()?;
-        assert_eq!(event_1.data(), event.data());
+        assert_eq!(EVENT_1.data(), event.data());
         assert_eq!(1, **event.position());
 
         assert_none!(events.next());
     }
 
     Ok(())
+}
+
+#[test]
+fn multiple_specifiers_with_range() -> Result<(), Error> {
+    let stream = stream(eventric_core::temp_path(), true)?;
+    let cache = Cache::default();
+
+    let specifier_0 = Specifier::new(EVENT_0.identifier().clone(), None);
+    let range = Version::new(1)..Version::MAX;
+    let specifier_1 = Specifier::new(EVENT_2.identifier().clone(), Some(range));
+    let selector = Selector::specifiers([specifier_0, specifier_1])?;
+    let query = Query::new([selector])?;
+    let condition = Condition::default().matches(&query);
+
+    let mut events = stream.query(&condition, &cache, None);
+
+    // A query with multiple specifiers (some of which have version ranges) should
+    // again return events matching any of the specifiers
+
+    {
+        let event = assert_some_as_result!(events.next()).unwrap()?;
+        assert_eq!(EVENT_0.data(), event.data());
+        assert_eq!(0, **event.position());
+
+        let event = assert_some_as_result!(events.next()).unwrap()?;
+        assert_eq!(EVENT_3.data(), event.data());
+        assert_eq!(3, **event.position());
+
+        assert_none!(events.next());
+    }
+
+    Ok(())
+}
+
+#[test]
+fn tag() -> Result<(), Error> {
+    let stream = stream(eventric_core::temp_path(), true)?;
+    let cache = Cache::default();
+
+    let tag_1 = EVENT_0.tags()[0].clone();
+    let selector = Selector::tags([tag_1])?;
+    let query = Query::new([selector])?;
+    let condition = Condition::default().matches(&query);
+
+    let mut events = stream.query(&condition, &cache, None);
+
+    // A query with a single tag should return only events matching that tag, in
+    // this case the first event
+
+    {
+        let event = assert_some_as_result!(events.next()).unwrap()?;
+        assert_eq!(EVENT_0.data(), event.data());
+        assert_eq!(0, **event.position());
+
+        assert_none!(events.next());
+    }
+
+    Ok(())
+}
+
+#[test]
+fn multiple_tags() -> Result<(), Error> {
+    let stream = stream(eventric_core::temp_path(), true)?;
+    let cache = Cache::default();
+
+    let tag_4 = EVENT_3.tags()[0].clone();
+    let tag_5 = EVENT_3.tags()[1].clone();
+    let selector = Selector::tags([tag_4, tag_5])?;
+    let query = Query::new([selector])?;
+    let condition = Condition::default().matches(&query);
+
+    let mut events = stream.query(&condition, &cache, None);
+
+    // A query with multiple tags should return only events matching all tags, in
+    // this case the last 2 events
+
+    {
+        let event = assert_some_as_result!(events.next()).unwrap()?;
+        assert_eq!(EVENT_2.data(), event.data());
+        assert_eq!(2, **event.position());
+
+        let event = assert_some_as_result!(events.next()).unwrap()?;
+        assert_eq!(EVENT_3.data(), event.data());
+        assert_eq!(3, **event.position());
+
+        assert_none!(events.next());
+    }
+
+    Ok(())
+}
+
+// -------------------------------------------------------------------------------------------------
+
+// Test Events
+
+static EVENT_0: LazyLock<EphemeralEvent> = LazyLock::new(|| {
+    let event = || {
+        Ok::<EphemeralEvent, Error>(EphemeralEvent::new(
+            Data::new("data_a")?,
+            Identifier::new("id_a")?,
+            [Tag::new("tag_1")?, Tag::new("tag_2")?, Tag::new("tag_3")?],
+            Version::new(0),
+        ))
+    };
+
+    event().unwrap()
+});
+
+static EVENT_1: LazyLock<EphemeralEvent> = LazyLock::new(|| {
+    let event = || {
+        Ok::<EphemeralEvent, Error>(EphemeralEvent::new(
+            Data::new("data_b")?,
+            Identifier::new("id_b")?,
+            [Tag::new("tag_2")?, Tag::new("tag_3")?, Tag::new("tag_4")?],
+            Version::new(1),
+        ))
+    };
+
+    event().unwrap()
+});
+
+static EVENT_2: LazyLock<EphemeralEvent> = LazyLock::new(|| {
+    let event = || {
+        Ok::<EphemeralEvent, Error>(EphemeralEvent::new(
+            Data::new("data_c")?,
+            Identifier::new("id_c")?,
+            [Tag::new("tag_3")?, Tag::new("tag_4")?, Tag::new("tag_5")?],
+            Version::new(0),
+        ))
+    };
+
+    event().unwrap()
+});
+
+static EVENT_3: LazyLock<EphemeralEvent> = LazyLock::new(|| {
+    let event = || {
+        Ok::<EphemeralEvent, Error>(EphemeralEvent::new(
+            Data::new("data_d")?,
+            Identifier::new("id_c")?,
+            [Tag::new("tag_4")?, Tag::new("tag_5")?, Tag::new("tag_6")?],
+            Version::new(1),
+        ))
+    };
+
+    event().unwrap()
+});
+
+// -------------------------------------------------------------------------------------------------
+
+// Test Stream
+
+fn stream<P>(path: P, populate: bool) -> Result<Stream, Error>
+where
+    P: AsRef<Path>,
+{
+    let mut stream = Stream::builder(path).temporary(true).open()?;
+
+    if populate {
+        stream.append([&*EVENT_0, &*EVENT_1, &*EVENT_2, &*EVENT_3], None)?;
+    }
+
+    Ok(stream)
 }
